625c5116d3370cb8d9e78a9f5c9be6ba232ae7ea
Artem Ivanov <arivanov.exceedteam@gmail.com>
2022-12-29 15:43:46 +0300

    new func for future

--- helpers/response.helper.go
+++ helpers/response.helper.go
@@ -8,6 +8,13 @@ type errorResponse struct {
 	Message string
 }
 
+func PrepareEntityResponse[T any](data T) any {
+	var res any
+	dataStr, _ := JsonMarshalIgnoreTags(data)
+	json.Unmarshal(dataStr, &res)
+	return res
+}
+
 func PrepareListResponse[T any](data []T) []any {
 	var res []any
 	for _, item := range data {
